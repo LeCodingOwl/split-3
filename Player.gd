3c3b46e5c6a2fbcbc552375e61985e361b65953c
jible <jamezman88th@gmail.com>
2024-05-17 03:39:19 -0700

    turns and bouncing fully implemented

--- Player.gd
+++ Player.gd
@@ -14,10 +14,12 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var bullet_spawn_point = $Neck/Camera3D/BulletSpawn
 var myTurn
 var turnManager
+var shot
 
 #var direction = Vector3.ZERO
 
 func _ready():
+	shot = true;
 	position.x = randi() % 18
 	position.z = randi() % 18
 	myTurn = false
@@ -26,19 +28,19 @@ func _ready():
 
 func _unhandled_input(event: InputEvent) -> void:
 	#Check for mouse input to lock camera to mouse
+	
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#Press the esc key to stop using camera
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	#Gets the mouses rotation to set the axis for the camera
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 	if (myTurn):
-		if event is InputEventMouseButton:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		#Press the esc key to stop using camera
-		elif event.is_action_pressed("ui_cancel"):
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		#Gets the mouses rotation to set the axis for the camera
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			if event is InputEventMouseMotion:
-				neck.rotate_y(-event.relative.x * 0.01)
-				camera.rotate_x(-event.relative.y * 0.01)
-				camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
-		
 		#Gets input map "shoot" and calls the the function to spawn a bullet
 		if Input.is_action_just_pressed("Shoot"):
 			#print("Fire")
--- models/bullet.gd
+++ models/bullet.gd
@@ -2,29 +2,34 @@ extends RigidBody3D
 
 var velocity = Vector3()
 var bounce_factor = 1.0
-
-# Called when the node enters the scene tree for the first time.
 var turnManager
+var maxBounces = 3
+var currentBounces
 
 func _ready():
+	currentBounces = 0
 	turnManager = $".."
 
-func _integrate_forces(state: PhysicsDirectBodyState3D):
-	# Apply the velocity to the bullet
-	state.linear_velocity = velocity
-	
-	for collision in state.get_contact_count():
-		var collision_normal = state.get_contact_normal(collision)
+func _physics_process(delta):
+	# Move the bullet and check for collisions
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		# Ensure collision_info is not null and get the collision normal
+		var collision_normal = collision_info.get_normal()
+		# Calculate the bounced velocity
 		velocity = velocity.bounce(collision_normal) * bounce_factor
+		
+		currentBounces+= 1
+		if (currentBounces >= maxBounces):
+			removeBullet()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_timer_timeout():
 	# Clears the bullet from the branch
 	removeBullet()
-	
+
 func removeBullet():
 	turnManager.turnPass()
 	queue_free()
